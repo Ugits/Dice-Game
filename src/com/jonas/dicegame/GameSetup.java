ad684bf012a98b3155d66a432a35662cdd6f40ce
Jonas Ullvahn <ullvahn@gmail.com>
2023-09-24 20:22:15 +0200

    init GameSetup

---
+++ src/com/jonas/dicegame/GameSetup.java
@@ -0,0 +1,17 @@
+/**
+ * Game set up
+ * The class purpose is to set the foundation for the game.
+ * Quantity of dice, players
+ *
+ *
+ */
+package com.jonas.dicegame;
+
+public class GameSetup {
+    UserInput sc = new UserInput();
+
+    int numOfPlayers;
+    int numOfDice;
+
+
+}
