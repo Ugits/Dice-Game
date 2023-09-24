77ae2e25acb1054ff96d56e933925b4f55c46920
Jonas Ullvahn <ullvahn@gmail.com>
2023-09-24 20:25:49 +0200

    add constructor and getter/setter in GameSetup

--- src/com/jonas/dicegame/GameSetup.java
+++ src/com/jonas/dicegame/GameSetup.java
@@ -13,5 +13,44 @@ public class GameSetup {
     int numOfPlayers;
     int numOfDice;
 
+    /**
+     * Declares number of dice and players.
+     */
+    public GameSetup(){
+        //Todo call from userInput / make method
+        System.out.print("Number of players: ");
+        setNumOfPlayers(sc.tryNextInt());
 
+        System.out.print("Number of dice: ");
+        setNumOfDice(sc.tryNextInt());
+
+        System.out.println();
+    }
+
+    /**
+     * @return Number of players.
+     */
+    public int getNumOfPlayers() {
+        return numOfPlayers;
+    }
+    /**
+     * @return Number of dice.
+     */
+    public int getNumOfDice() {
+        return numOfDice;
+    }
+    /**
+     * Set number of players.
+     * @param numOfPlayers
+     */
+    public void setNumOfPlayers(int numOfPlayers) {
+        this.numOfPlayers = numOfPlayers;
+    }
+    /**
+     * Set number of dice
+     * @param numOfDice
+     */
+    public void setNumOfDice(int numOfDice) {
+        this.numOfDice = numOfDice;
+    }
 }
