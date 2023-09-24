08f44147196c981319a8f3e55cc2d0e3abda18a4
Jonas Ullvahn <ullvahn@gmail.com>
2023-09-24 20:18:18 +0200

    add constructor and getter/setter in PlayerClass

--- src/com/jonas/dicegame/Player.java
+++ src/com/jonas/dicegame/Player.java
@@ -6,5 +6,48 @@ public class Player {
     String name;
     int score;
     boolean winner;
-    
+
+    /**
+     * Declares player name och initialize score.
+     */
+    public Player (){
+        UserInput sc = new UserInput();
+
+        System.out.print("Enter you name: ");
+        setPlayerName(sc.userInput.nextLine());
+        setScore(0);
+    }
+
+    // getter-setter
+    public int getPlayerNum() {
+        return pNum;
+    }
+
+    public void setPlayerNum(int playerNum) {
+        this.pNum = playerNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.name = playerName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isWinner() {
+        return winner;
+    }
+
+    public void setWinner(boolean winner) {
+        this.winner = winner;
+    }
 }
